0fbe84b463cd6175d338ac828e517786784d849a
Jaheim Jackson <jaheim.dre.jackson@gmail.com>
2024-11-07 18:18:50 +0000

    Updated Portfolio fields

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Jaheim Jackson",
+  title: "Student & Aspiring Dev",
+  email: "",
+  gitHub: "Dre-J",
+  instagram: "jah.a.g",
+  linkedIn: "jaheim-jackson",
+  medium: "jaheim.dre.jackson",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
